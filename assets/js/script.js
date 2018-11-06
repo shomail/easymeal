5a4446cf95d35067ba876106e8ea7e3a0b259f85
Shomail <shomail1988@gmail.com>
2018-11-06 12:09:41 +0100

    added smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
+polyfill();
 const nav = document.querySelector('nav');
+const section = document.querySelector('.section-features');
 
-const navTop = nav.offsetTop;
+const sectionTop = section.offsetTop;
 
-function fixNav() {
-  console.log(navTop);
+function stickyNav() {
+  if(window.scrollY >= section.offsetTop - 20) {
+    nav.classList.add('sticky')
+  }else {
+    nav.classList.remove('sticky')
+  }
 }
 
-window.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', stickyNav);
+
+function smoothScroll(element) {
+  element.preventDefault();
+  
+  const targetName = element.target.getAttribute('data-target');
+
+  const targetElem = document.querySelector(`.${targetName}`);
+  
+  window.scrollTo({
+    top: targetElem.offsetTop - 10,
+    behavior: 'smooth'
+  });
+}
+
+const btnLinks = document.querySelectorAll('.btn-link');
+
+btnLinks.forEach(elem => elem.addEventListener('click', smoothScroll))
