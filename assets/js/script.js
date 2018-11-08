3382c35d3c68b97ec95a5d14dc4a2f72d0fea5eb
Shomail <shomail1988@gmail.com>
2018-11-08 22:34:45 +0100

    mobile navigation styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,34 @@
 polyfill();
 
 function stickyNav() {
-
   const nav = document.querySelector('nav');
   const hiddenFeatures = document.querySelector('.hidden-features');
   const section = document.querySelector('.section-features');
 
-  if(window.scrollY >= section.offsetTop - 150) {
+  if (window.scrollY >= section.offsetTop - 150) {
     hiddenFeatures.classList.add('show-features');
   }
-  
-  if(window.scrollY >= section.offsetTop - 20) {
-    nav.classList.add('sticky')
-  }else {
-    nav.classList.remove('sticky')
+
+  if (window.scrollY >= section.offsetTop - 50) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
   }
 }
 
 function smoothScroll(element) {
   element.preventDefault();
-  
+
+  document.querySelector('.main-nav').style.right = '-100vw';
+  document.querySelector('.mobile-nav-icon i').classList.remove('fa-times');
+  document.querySelector('.mobile-nav-icon i').classList.add('fa-bars');
+
   const targetName = element.target.getAttribute('data-target');
 
   const targetElem = document.querySelector(`.${targetName}`);
-  
+
   window.scrollTo({
-    top: targetElem.offsetTop - 10,
+    top: targetElem.offsetTop - 50,
     behavior: 'smooth'
   });
 }
@@ -33,12 +36,13 @@ function smoothScroll(element) {
 function toggleNav() {
   const nav = document.querySelector('.main-nav');
   const btn = document.querySelector('.mobile-nav-icon i');
-  if(nav.style.display === 'none' || !nav.style.display) {
-    nav.style.display = 'block';
+  console.log(nav.style.right);
+  if (nav.style.right === '-100vw' || !nav.style.right) {
+    nav.style.right = '0';
     btn.classList.remove('fa-bars');
     btn.classList.add('fa-times');
-  }else{
-    nav.style.display = 'none';
+  } else {
+    nav.style.right = '-100vw';
     btn.classList.remove('fa-times');
     btn.classList.add('fa-bars');
   }
@@ -48,9 +52,8 @@ window.addEventListener('scroll', stickyNav);
 
 const btnLinks = document.querySelectorAll('.btn-link');
 
-btnLinks.forEach(elem => elem.addEventListener('click', smoothScroll))
+btnLinks.forEach(elem => elem.addEventListener('click', smoothScroll));
 
 const mobileNavBtn = document.querySelector('.mobile-nav-icon');
 
-mobileNavBtn.addEventListener('click', toggleNav)
-
+mobileNavBtn.addEventListener('click', toggleNav);
